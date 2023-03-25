cef48e5c823de86f53ad15c658a71e402de545a3
[Moises Almeida] <[moisesssprg@gmail.com]>
2023-03-25 18:01:43 -0300

    project created

---
+++ backend/src/main/java/com/spring/fxmovie/FxmovieApplication.java
@@ -0,0 +1,13 @@
+package com.spring.fxmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/spring/fxmovie/FxmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.fxmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
