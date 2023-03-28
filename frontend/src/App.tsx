e3f8918dd377c25193d9d5055235633aa2d38a78
[Moises Almeida] <[moisesssprg@gmail.com]>
2023-03-27 23:41:55 -0300

    routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
-
-import NavBar from "./components/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Listing from "pages/Listing";
+import Form from "pages/Form";
+import Header from "components/header";
+import Navbar from "components/Navbar";
 
 function App() {
   return (
-      <NavBar />
-
+    <BrowserRouter>
+      <Navbar />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/header/index.tsx
@@ -0,0 +1,55 @@
+import topGunImage from "assets/img/top-gun.png";
+import iconSeta from "assets/img/icons8-divisa-circulada-à-direita-64.png";
+import "./styles.css";
+
+function Header() {
+  return (
+    <header>
+      <div className="container-header">
+        <div className="container-blue">
+          {/* <img src= {topGunImage} alt="" /> */}
+
+          <div
+            id="carouselExampleSlidesOnly"
+            className="carousel slide"
+            data-bs-ride="carousel"
+          >
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img src={topGunImage} alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src={topGunImage} alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src={topGunImage} alt="..." />
+              </div>
+            </div>
+          </div>
+
+          <a href="#">ver avaliações</a>
+        </div>
+        <div className="container-pink">
+          <section className="title">
+            Avalie, compartilhe e descubra o poder do cinema e da televisão em
+            nosso site de avaliação de filmes e séries.
+          </section>
+
+          <a href="#">
+            Criar conta <img src={iconSeta} alt="" />
+          </a>
+
+          <p>
+            Não sabe o que assistir? Nosso site de avaliação de filmes e séries
+            tem as melhores recomendações dos espectadores como você. Encontre
+            seu próximo vício de streaming agora mesmo!
+          </p>
+        </div>
+      </div>
+
+      <h2>FX - Favorite experiences -</h2>
+    </header>
+  );
+}
+
+export default Header;
